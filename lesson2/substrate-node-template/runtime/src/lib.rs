445d716c46a0d28107c8182f66fd10a6ad6d3d98
w5pand <w5pand@163.com>
2020-06-07 09:28:42 +0000

    lesson-2

--- lesson2/substrate-node-template/runtime/src/lib.rs
+++ lesson2/substrate-node-template/runtime/src/lib.rs
@@ -212,6 +212,7 @@ impl grandpa::Trait for Runtime {
 
 parameter_types! {
 	pub const MinimumPeriod: u64 = SLOT_DURATION / 2;
+	pub const MaxClaimLength: u32 = 6;
 }
 
 impl timestamp::Trait for Runtime {
@@ -256,6 +257,11 @@ impl sudo::Trait for Runtime {
 impl template::Trait for Runtime {
 	type Event = Event;
 }
+///poe
+impl poe::Trait for Runtime {
+	type Event = Event;
+	type MaxClaimLength = MaxClaimLength;
+}
 
 construct_runtime!(
 	pub enum Runtime where
@@ -273,6 +279,7 @@ construct_runtime!(
 		Sudo: sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Used for the module template in `./template.rs`
 		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		PoeModule: poe::{Module, Call, Storage, Event<T>},
 	}
 );
 
